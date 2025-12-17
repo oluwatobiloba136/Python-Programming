80815714fb0022087a728c0fd65291597038d06f
oluwatobiloba136 <tobi.kazeem@outlook.com>
2025-12-17 07:54:55 -0700

    FUNCTIONAL BASED CLASS

--- api_basics/api/views.py
+++ api_basics/api/views.py
@@ -1,12 +1,54 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
+from students.models import Student
+from .serializers import StudentSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
 # Create your views here.
 
+
+@api_view(['GET', 'POST'])
 def studentsView(request):
-    students ={
-        'id':1,
-        'name': 'Kelly',
-        'class':'D365'
-    }
-    return JsonResponse(students)
+    # students =Student.objects.all()
+    # students_list = list(students.values()) # manual serialization
+    # return JsonResponse(students_list, safe=False)
+
+
+    # function based views
+    if request.method == 'GET':
+        # GET ALL DATA FROM STUDENT TABLE
+        students  = Student.objects.all()
+        serializer = StudentSerializer(students, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+@api_view(['GET', 'PUT','DELETE'])
+def studentDetailView(request, pk):
+    try:
+        student = Student.objects.get(pk=pk)
+    except Student.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = StudentSerializer(student)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    elif request.method == 'PUT':
+        serializer = StudentSerializer(student, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+    elif request.method == 'DELETE':
+        student.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
